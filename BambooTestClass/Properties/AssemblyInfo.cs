63d86c0c8ef33c68828c4ace99a19ab0c4970c46
ali0311 <mdali0311@gmail.com>
2020-09-05 21:18:30 +0530

    test

---
+++ BambooTestClass/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BambooTestClass")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BambooTestClass")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4ac106d3-911d-4f11-96c6-fa9666f37ee9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BambooTestClass/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace BambooTestClass
+{
+    [TestClass]
+    public class BambooTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver;
+
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.in/");
+            driver.Close();
+
+        }
+    }
+}
